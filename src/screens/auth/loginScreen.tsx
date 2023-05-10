949965744b60617fa9965eb52be3c7bd39e9f036
Friday Davis <55281305+sirdavis99@users.noreply.github.com>
2023-05-10 11:36:54 +0100

    setup for Heifer

---
+++ src/screens/auth/loginScreen.tsx
@@ -0,0 +1,7 @@
+export const LoginScreen = ( ) => {
+    return (
+        <div>
+            Login Screen
+        </div>
+    )
+}
---
+++ src/theme/chakra.config.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const chakraCustomTheme = extendTheme({
+    fonts: {
+        heading: "Montserrat",
+        body: "Montserrat",
+    },
+})
