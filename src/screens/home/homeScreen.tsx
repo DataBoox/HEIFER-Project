5bd30fe19e19e93e86fb9cf8a0b2884abf94396c
Oluseyi18 <seyeezi18@gmail.com>
2023-05-14 00:09:13 +0100

    Davidoo

--- src/screens/home/homeScreen.tsx
+++ src/screens/home/homeScreen.tsx
@@ -4,8 +4,8 @@ import { PrimaryLoader } from 'components';
 import React, { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useAuth } from 'store/auth/hooks';
-
 import { AdminMenuLinkItem, NavHeader, SidebarMenu as DashboardSidebarMenu } from './components';
+
 
 export const HomeScreen = () => {
     const { user } = useAuth();
@@ -15,15 +15,15 @@ export const HomeScreen = () => {
     const windowWidth = useWindowWidth()
     let sidebarMenu = AdminMenuLinkItem;
 
-    // useEffect(() => {
-    //     let redirectTo: string | null = null;
-    //     setTimeout(() => {
-    //         if (user && !user?.email_verification) redirectTo = "/verify/email";
-    //         if (!user) redirectTo = "/login";
-    //         if (redirectTo) navigate(redirectTo)
-    //         setIsLoading(false);
-    //     }, 2000)
-    // });
+    useEffect(() => {
+        let redirectTo: string | null = null;
+        setTimeout(() => {
+            // if (user && !user?.email_verification) redirectTo = "/verify/email";
+            // if (!user) redirectTo = "/login";
+            // if (redirectTo) navigate(redirectTo)
+            setIsLoading(false);
+        }, 2000)
+    });
 
     return (
       <Box minH="100vh" bg={"#F4F5F6"}>
