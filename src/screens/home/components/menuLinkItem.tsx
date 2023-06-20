4deac7a524c63f9a4bd6f6fce046b45c56dd8108
Oluseyi18 <seyeezi18@gmail.com>
2023-06-20 21:31:12 +0100

    Filter systems 2

--- src/screens/home/components/menuLinkItem.tsx
+++ src/screens/home/components/menuLinkItem.tsx
@@ -17,20 +17,6 @@ import { LinkItemProps } from "../interface";
 export const AdminMenuLinkItem: Array<LinkItemProps> = [
   { name: "Dashboard", icon: RiHomeFill, path: "/" },
   { name: "Maps", icon: MdMyLocation, path: "maps" },
-  // {
-  //     name: "People", icon: MdGroups, dropdown: [
-  //         { name: "Members", icon: BsFillPeopleFill, path: "members" },
-  //         { name: "Archived Members", icon: FaArchive, path: "archived" },
-  //         { name: "First Timers", icon: BsPersonPlusFill, path: "first-timers" },
-  //         { name: "Departments", icon: FaChurch, path: "departments" },
-  //         { name: "Groups", icon: FiUsers, path: "groups" },
-  //         { name: "Prayer Requests", icon: FaPray, path: "prayer-requests" },
-  //         { name: "Testimony", icon: FaHands, path: "testimony" }
-  //     ]
-  // },
-  // { name: "Projects", icon: FaShapes, path: "projects" },
-  // { name: "Create Form", icon: MdNoteAdd, path: "create-form" },
-  // { name: "Register Household", icon: HiUserAdd, path: "register" },
   { name: "Households", icon: GiFarmer, path: "farmers" },
   { name: "Groups", icon: BsGridFill, path: "groups" },
   { name: "Interventions", icon: BsFillPatchCheckFill, path: "interventions" },
